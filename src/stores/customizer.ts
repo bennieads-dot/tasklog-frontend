cfe5ed39b54d5d6177531b0f78c306935b5c88c9
bennieads-dot <benjamin.j.adams144@gmail.com>
2022-10-09 13:37:48 -0400

    clean

--- src/stores/customizer.ts
+++ src/stores/customizer.ts
@@ -16,7 +16,7 @@ export const useCustomizerStore = defineStore({
     Sidebar_drawer: null,
     Customizer_drawer: false,
     SidebarColor: "white", //Change Sidebar Color || 'white', | "#2b2b2b" | "rgb(44, 59, 164)" | "rgb(96, 44, 164)" | "rgb(151, 210, 219)" | "rgb(77, 86, 100)"
-    mini_sidebar: true,
+    mini_sidebar: false,
     navbarColor: "#f6f6f6",
     setHorizontalLayout: false, // Horizontal layout
     darktheme: false,
